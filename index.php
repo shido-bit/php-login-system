8f3a91d7eb891d6b7a6c16230b7f8c985ba408b2
shido-bit <rohan0565rathore@gmail.com>
2019-07-05 14:28:56 +0530

    second commit

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
-<?php 
-echo "hello world";
-?>
+<!DOCYPE html>
+<html>
+<head>
+<!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.6/css/uikit.min.css" />
+
+<!-- UIkit JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.6/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.6/js/uikit-icons.min.js"></script>	
+</head>
+<body>
+<form class="uk-form-stacked">
+
+    <div class="uk-margin">
+        <label class="uk-form-label" for="form-stacked-text">Email</label>
+        <div class="uk-form-controls">
+            <input class="uk-input" id="form-stacked-text" required="required" type="text" placeholder="abcd@efg.com">
+        </div>
+    </div>
+
+
+    <div class="uk-margin">
+        <label class="uk-form-label" for="form-stacked-text">password</label>
+        <div class="uk-form-controls">
+            <input class="uk-input" id="form-stacked-text" required="required" type="password" placeholder="Your password">
+        </div>
+    </div>
+   <div class="uk-margin">
+        <button class="uk.button uk.button.default" type="submit">LOGIN</button>
+    </div>
+
+   
+
+</form>
+</body>
+</html>
