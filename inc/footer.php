fc19accf17db5c6f74881dd84b4b2dc38a2d5751
shido-bit <rohan0565rathore@gmail.com>
2019-07-08 21:23:28 +0530

    modified file

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,10 @@
+<!-- jQuery is required -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 <!-- UIkit JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.6/js/uikit.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.6/js/uikit-icons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit-icons.min.js"></script>
+
+<script src="php_login_course/assests/js/main1.js">
+
+</script>
