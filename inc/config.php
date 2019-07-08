192592091094cc68a8b5c2fc4989434401cc1c45
shido-bit <rohan0565rathore@gmail.com>
2019-07-08 21:18:31 +0530

    modified file

--- inc/config.php
+++ inc/config.php
@@ -1,9 +1,17 @@
 <?php 
- 
-// If ther is no constant defined called _CONFIG_ do not load this file.
- if(!defined('_CONFIG_')) {
-    exit('you don ot have a config');
- }
 
- ?>
+	// If there is no constant defined called __CONFIG__, do not load this file 
+	if(!defined('_CONFIG_')) {
+		exit('You do not have a config file');
+	}
 
+
+	// Our config is below
+
+
+	// Include the DB.php file;
+	include_once "classes/DB.php";
+
+	$con = DB::getConnection();
+
+?>
